b4e154f76e67a8b74704e8f1f8f6dab4223e4fa0
gzt <gzt@iastate.edu>
2019-07-01 15:22:03 -0500

    add pkg infrastructure

---
+++ R/rNFFT.R
@@ -0,0 +1,16 @@
+##
+## rNFFT.R - glue between user and rnfft.c
+##
+## Authors:
+##  Geoffrey Thompson <gzthompson@gmail.com>
+
+
+#' R wrapper for the NFFT library (nonequispaced nodes Fast Fourier Transform)
+#'
+#' Provides a wrapper around the NFFT 3.0 C library, which implements
+#' the fast Fourier transform for non-equispaced nodes, its adjoint, and its
+#' inversion. This provides functions for the NFFT and Solver routines.
+#'
+#' @docType package
+#' @name rNFFT
+NULL
