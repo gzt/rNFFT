0b7a234d78a0a24120a9328cb21492c744b223ee
gzt <gzthompson@gmail.com>
2020-04-05 00:54:55 -0500

    fix PET reference

--- R/data.R
+++ R/data.R
@@ -2,5 +2,5 @@
 #' PET Phantom Example
 #'
 #' @format A \eqn{257 \times 257}{257 x 257} matrix representing a
-#' PET phantom. From the output of [PET::phantom].
+#' PET phantom. From the output of the defunct PET package.
 "phantom_example"
